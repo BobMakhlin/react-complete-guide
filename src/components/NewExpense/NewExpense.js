d96f6017ad9c22f9a07a90b09edaad7dd92dd468
BobMakhlin <borismakhlin5@gmail.com>
2022-10-17 22:03:23 +0300

    Enhance UI/UX of the NewExpense

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,35 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
 
 const NewExpense = (props) => {
+  const [creationModeActive, setCreationModeAction] = useState(false);
+
   const submitHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setCreationModeAction(false);
+  };
+
+  const cancellationHandler = () => {
+    setCreationModeAction(false);
+  };
+  
+  const addButtonClickHandler = () => {
+    setCreationModeAction(true);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSubmit={submitHandler} />
+      {!creationModeActive && (
+        <button onClick={addButtonClickHandler}>Add New Expense</button>
+      )}
+      {creationModeActive && (
+        <ExpenseForm onSubmit={submitHandler} onCancel={cancellationHandler} />
+      )}
     </div>
   );
 };
